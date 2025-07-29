5ddbd082bbec89a3c97c48124d415c6c12aacac8
mentasuave01 <mentasuave01@gmail.com>
2025-07-29 15:07:21 +0200

    Refactor Vite configuration: Update import order and formatting for consistency. Remove unnecessary minification setting.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-oxc'
+import react from "@vitejs/plugin-react-oxc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'esnext',
-    minify: 'terser'
-  }
-})
+	plugins: [react()],
+	build: {
+		target: "esnext",
+	},
+});
